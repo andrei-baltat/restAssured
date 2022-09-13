b887b5e040041d475afec72e46f72176702c8e4f
andrei.baltat <andrei.baltat@adswizz.com>
2022-09-13 07:47:04 +0300

    Initial commit

---
+++ src/main/java/files/Basics.java
@@ -0,0 +1,21 @@
+package files;
+
+import files.ReUsableMethods;
+import files.payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Basics {
+
+	public static void main(String[] args) {
+
+	
+
+		
+	}
+
+}
---
+++ src/test/java/files/MyClassTest.java
@@ -0,0 +1,7 @@
+package files;
+
+import static org.testng.Assert.*;
+
+public class MyClassTest {
+
+}
